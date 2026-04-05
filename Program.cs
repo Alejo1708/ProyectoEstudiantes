cada8476355da0876226c9ef70c973b22b1ba82b
Alejo1708 <alejo20081701@gmail.com>
2026-04-04 21:07:47 -0500

    Implemento las opciones del menu principal

--- Program.cs
+++ Program.cs
@@ -26,3 +26,33 @@ class Program
 
            // Se lee la opcion del usuario
             opcion = int.Parse(Console.ReadLine());
+
+            
+             // Se evalua la opcion elegida
+            switch (opcion)
+            {
+                case 1:      // Llama al metodo para agregar un estudiante
+                    lista.Agregar();
+                    break;
+
+                case 2:     // Muestra todos los estudiantes
+                    lista.Listar();
+                    break;
+
+                case 3:     // Permite buscar un estudiante por codigo
+                    Console.Write("Codigo: ");
+                    int cod = int.Parse(Console.ReadLine());
+                    var est = lista.Buscar(cod);
+
+                    
+                    // Se verifica si el estudiante existe
+                    if (est != null)
+                        Console.WriteLine(est.Nombre + " encontrado.");
+                    else
+                        Console.WriteLine("No existe.");
+                    break;
+
+                case 4:         // Permite eliminar un estudiante por codigo
+                    Console.Write("Codigo: ");
+                    lista.Eliminar(int.Parse(Console.ReadLine()));
+                    break;
